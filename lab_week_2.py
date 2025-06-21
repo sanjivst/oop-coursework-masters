858f0a58d6b9266d043e798e8037ddc346db9a80
sanjivst <sanjeev.shrestha999@gmail.com>
2025-06-21 23:23:37 +0100

    Lab Week 2 Completed with basic Python Syntax Learning

--- lab_week_2.py
+++ lab_week_2.py
@@ -1,25 +1,170 @@
+print("----------------------------------------------------")
 # Comparison and Conditionals
 # Comparison Operators
-# Logical Operators
-# If Conditional
-# If else conditionals
-# If elif else conditionals
-# Summary Tasks
+x = 7
+y = 2
 
+# equal operator
+print(x == y)
+
+# not equal operator
+print(x != y)
+
+# grater than operator
+print(x > y)
+
+# smaller than operator
+print(x < y)
+
+# greater than or equal to	
+print(x >= y)
+
+# smaller than or equal to	
+print(x <= y)
+print("----------------------------------------------------")
+# Logical Operators
+x = 8
+
+# and operator
+print(x > 5 and x < 9)
+
+# or operator
+print(x > 5 or x < 9)
+
+# not operator
+print(not(x > 5 and x < 9))
+print("----------------------------------------------------")
+# If Conditional
+
+age = 19    #15
+age_group = "child"
+if age > 18:
+        age_group = "adult"
+        print(f"The age group is {age_group}")
+
+# If else conditionals
+
+wind_speed = 30 # 5
+if wind_speed < 10:
+    print("It is a calm day")
+else:
+    print("It is a windy day")
+
+# If elif else conditionals
+
+grade = 55 # 47 68 73
+if grade < 50:
+    print("You failed")
+elif grade < 60:
+    print("You passed")
+elif grade < 70:
+    print("You got a good pass")
+else:
+    print("You got an excellent pass")
+print("----------------------------------------------------")
+# Summary Tasks
+temperature1 = 56
+temperature2 = 27
+if temperature1 == temperature2:
+     print("Temperature 1 and Temperature 2 are same.")
+else:
+     print("Temperature 1 and Temperature 2 are not same.")
+print("----------------------------------------------------")
 # Python Lists
 # 1. Creating a list
-# 2. Accessing a List
-# 3. Modifying a List
-# 4. Summary Task
+city_list = ["Glasgow", "London", "Edinburgh"]
 
+# 2. Accessing a List
+print(city_list[2])
+print(city_list[1:3])
+
+# 3. Modifying a List
+city_list.append("Manchester")
+city_list[1] = "Birmingham"
+print("----------------------------------------------------")
+# 4. Summary Task
+colours = ["Purple", "Magenta", "Yellow"]
+print(colours)
+
+select_color = colours[1]
+print(select_color)
+
+colours[0] = "Cyan"
+print(colours)
+print(len(colours))
+
+if "red" in colours:
+    print("Red is in the list")
+
+selected_color = colours[1:3]
+print(selected_color)
+
+print("----------------------------------------------------")
 # Python Loops
 # 1. While Loops
+
 # 2. For Loops
+for city in city_list:
+    print(city)
+
 # 3. Loop Keywords: Range, Break and continue
+for i in range(4):
+    if i == 3:
+        break
+    print(i)
+print("----------------------------------------------------")
 # 4. Summary Tasks
-
+# Even Numbers
+numbers = list(range(1, 11))
+print(numbers)
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
+print("----------------------------------------------------")
+# Sum of Squares
+sum_of_squares = 0
+for num in list(range(1,6)):
+    sum_of_squares += num ** 2
+print(sum_of_squares)
+print("----------------------------------------------------")
+# Countdown
+countdown = 10
+while countdown > 0:
+    print(countdown)
+    countdown -= 1
+print("lift off!")
+print("----------------------------------------------------")
 # Obtain User Input
+# User Input and Conditional Statements
+age = int(input('Enter your Age:'))
+if age < 18:
+    print("You are a minor.")
+elif age < 65:  
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
+print("----------------------------------------------------")
+# Temprerature Convertor
+celsius_input = float(input("Enter the value of temperature in Celsius:"))
+print("The temperature you have entered is", celsius_input, "degree celsius")
 
-# Task 1 Age Categorisation
-age = input('Enter your Age:')
-age = int(age)
+print("What temperature you want to convert?")
+print("1. Fahrenheit")
+print("2. Kelvin")
+print("3. Both")
+input_choice = input("Enter your choice (1/2/3): ")
+if input_choice not in ['1', '2', '3']:
+    print("Invalid choice. Please enter 1, 2, or 3.")
+    exit()
+elif input_choice == '1':
+    degree_f = float(((celsius_input * 9)/5) + 32)
+    print(celsius_input, "degree Celsius is equal to", degree_f, "Fahrenheit")
+elif input_choice == '2':
+    degree_k = float(celsius_input + 273.15)
+    print(celsius_input, "degree Celsius is equal to", degree_k, "Kelvin")
+elif input_choice == '3':
+    degree_f = float(((celsius_input * 9)/5) + 32)
+    print(celsius_input, "degree Celsius is equal to", degree_f, "Fahrenheit")
+    degree_k = float(celsius_input + 273.15)
+    print(celsius_input, "degree Celsius is equal to", degree_k, "Kelvin")
+print("----------------------------------------------------")
--- lab_week_4.py
+++ lab_week_4.py
@@ -1,6 +1,69 @@
 class TaskList:
     def __init__(self, owner):
-        self.owner = owner
+        self.owner = owner.upper()
         self.tasks = []
-my_task_list = TaskList("John")
-print(my_task_list.owner)
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def remove_task(self, ix):
+        try:
+            del self.tasks[ix]
+            print(f"Task at index {ix} removed successfully.")
+        except IndexError:
+            print(f"No task found at index {ix}. Please enter a valid index.")
+
+    def view_tasks(self): 
+    # TODO: Add code here to view all tasks in the list 
+        for ix, task in enumerate(self.tasks):
+            print(ix, task)
+
+    def list_options(self):
+        while True: 
+            print("To-Do List Manager") 
+            print("1. Add a task") 
+            print("2. View tasks") 
+            print("3. Remove a task")
+            print("4. Change Title of Task")
+            print("5. Mark Task as Completed")
+            print("6. Quit") 
+             
+            choice = input("Enter your choice: ") 
+             
+            if choice == "1":
+                title = input("Enter a task: ")
+                task = Task(title) 
+                self.add_task(task)
+
+            elif choice == "2":
+                self.view_tasks()
+
+            elif choice == "3":
+                ix = int(input("Enter the index of the task to remove: "))
+                self.remove_task(ix)
+        
+            elif choice == "4":
+                break
+        
+class Task:
+    def __init__(self, title, completed):
+        self.title = title
+        self.completed = bool(completed)
+
+    def __str__(self):
+        return f"Task: {self.title}"   
+        return f"Task: {self.completed}"
+    
+    def mark_completed():
+        Task.self.completed = True
+    
+    def change_title(new_title):
+        Task.self.title =  new_title
+
+my_task_list = TaskList("Sanjiv")
+# This part is just to test the functionality by adding some tasks to the list
+my_task_list.tasks = [Task("Do Homework"), Task("Do Laundry"), Task("Go Shopping")]
+my_task_list.list_options()
+
+
+
